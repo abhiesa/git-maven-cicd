eb525a91ea2f54ef192b793b45f0fb677dc16d92
Abhishek Pandey <abhiesa@gmail.com>
2018-08-12 20:55:31 +0530

    added spring boot template

---
+++ src/main/java/com/abhiesa/gitmavencicd/GitMavenCicdApplication.java
@@ -0,0 +1,12 @@
+package com.abhiesa.gitmavencicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitMavenCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitMavenCicdApplication.class, args);
+	}
+}
